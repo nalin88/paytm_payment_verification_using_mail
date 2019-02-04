fe9e4c3c7876cdcfe944b2ac4f1c7b32d7b6a430
Nalin Nishant <36907019+nalin88@users.noreply.github.com>
2019-02-05 01:00:12 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,19 @@
+<?php
+$decided_fee = '1'; // decided registration fees - put here 
+
+//gmail Credentials
+$username = 'nalinnishant.020@gmail.com';
+$password = '****************';
+
+//datebase credentials
+$servername = "localhost";
+$db_username = "****";
+$db_password = "****";
+$db_name = "****";
+//table name
+$table = "payment";
+
+
+
+
+?>
